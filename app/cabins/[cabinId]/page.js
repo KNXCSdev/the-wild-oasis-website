efdd8fb4e4af1903f3fbf5e2f2d9bdeecb70cce5
KNXCSdev <michal.miksho@gmail.com>
2025-01-21 22:24:42 +0100

    Error boundaries

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -2,6 +2,17 @@ import { getCabin } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 
+// export const metadata = {
+//   title: "Cabin",
+// };
+
+export async function generateMetadata({ params }) {
+  const { cabinId } = await params;
+  const { name } = await getCabin(cabinId);
+
+  return { title: `Cabin ${name}` };
+}
+
 export default async function Page({ params }) {
   const { cabinId } = await params;
   const cabin = await getCabin(cabinId);
